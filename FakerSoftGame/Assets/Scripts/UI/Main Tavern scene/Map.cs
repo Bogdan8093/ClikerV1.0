fb120ad8e0cb7f3cfb908ac5fa2122971fdb9a62
Kir <kirov.igor.o@gmail.com>
2017-07-02 20:19:55 +0300

    Map update

--- FakerSoftGame/Assets/Scripts/UI/Main Tavern scene/Map.cs
+++ FakerSoftGame/Assets/Scripts/UI/Main Tavern scene/Map.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class Map : MonoBehaviour {
 
     public GameObject MapUI;
+    public GameObject CurrentUI;
 
     void OnMouseDown()
     {
         MapUI.SetActive(true);
+        if(CurrentUI != null)
+        {
+            CurrentUI.SetActive(false);
+        }
     }
 }
