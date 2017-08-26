001feec73515f464708fe54405677dbe6a8de8c1
DaRK <vlad94@ukr.net>
2017-08-26 11:56:16 +0300

    LoginSceneUI + Fonts

---
+++ FakerSoftGame/Assets/Scripts/Login/others/pingStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pingStatus : MonoBehaviour {
+    string URL = "s2s.epac.to";
+    public Text pingValue;
+    void Start () {
+        StartCoroutine (ping ());
+    }
+    IEnumerator ping () {
+        while (true) {
+            var dsa = new Ping (URL);
+            yield return new WaitUntil (() => dsa.isDone == true);
+            pingValue.text = "Ping: " + dsa.time + "ms";
+            yield return new WaitForSeconds (1);
+        }
+    }
+}
