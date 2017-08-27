8e9b45aef6979e59049cd7875ce5ea0d579a7cac
LAPTOP-R8B2GPLT\Lenovo <bogdanlikhoglyad@gmail.com>
2017-08-27 13:10:20 +0300

    Jornal + sprites

---
+++ FakerSoftGame/Assets/Scripts/UI/Journalscript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Journalscript : MonoBehaviour {
+    public GameObject Questtab;
+    public GameObject Collectiontab;
+    public GameObject Kodecstab;
+    public GameObject Achivmenttab;
+    // Use this for initialization
+    public void onquestclick ()
+    {
+        Collectiontab.SetActive(false);
+        Kodecstab.SetActive(false);
+        Achivmenttab.SetActive(false);
+        Questtab.SetActive(true);
+    }
+      public void onAchivmentclick ()
+    {
+        Collectiontab.SetActive(false);
+        Kodecstab.SetActive(false);
+        Achivmenttab.SetActive(true);
+        Questtab.SetActive(false);
+    }
+    public void onKodecsclick()
+    {
+        Collectiontab.SetActive(false);
+        Kodecstab.SetActive(true);
+        Achivmenttab.SetActive(false);
+        Questtab.SetActive(false);
+    }
+    public void onCollectionsclick()
+    {
+        Collectiontab.SetActive(true);
+        Kodecstab.SetActive(false);
+        Achivmenttab.SetActive(false);
+        Questtab.SetActive(false);
+    }
+}
